980e06fa715280245c3a5335633753f6e6d982f4
xjx20210817 <xjx>
2021-12-24 14:33:43 +0800

    runtime-width-compiler $mouont

--- src/platforms/web/entry-runtime-with-compiler.js
+++ src/platforms/web/entry-runtime-with-compiler.js
@@ -4,7 +4,28 @@ import { query } from './utils/index'
 Vue.prototype.$mount = function (el,hydrating) {
     console.log('mountd gsd',el)
     el = el && query(el)
-    console.log(el)
+    const options = this.$options
+    if (!options.render) {
+        let template = options.template
+        if (template) {
+            if (typeof template === 'string') {
+                if (template.charAt(0) === '#') {
+
+                }
+            } else if (template.nodeType) {
+
+            } else {
+
+            }
+        } else if (el) {
+
+        }
+    }
+
+    if (template) {
+      const { render, staticRenderFns } = compileToFunctions(template, {
+        
+    }
 }
 
 
--- src/platforms/web/entry-runtime.js
+++ src/platforms/web/entry-runtime.js
@@ -1,1 +1,3 @@
+import Vue from "./runtime"
+
 console.log('entry-runtime')
